2d5093c63efa7577446d8797060a8c3ea523b746
Zacsch <a1668393@student.adelaide.edu.au>
2018-05-04 14:56:53 +0930

    added more files

---
+++ practical-07-11/player.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "player.h"
+
+using namespace std;
+
+player::player()
+{
+
+}
+
+void player::hit_me()
+{
+
+}
+
+int player::sit()
+{
+
+}
+
+int player::dealer_swap()
+{
+
+}
+
+player::~player()
+{
+}
--- practical-07-11/player.h
+++ practical-07-11/player.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include "player_setup.h"
 
 using namespace std;
 
@@ -14,7 +15,7 @@ class player: public player_setup
 		bool winner_winner_chicken_dinner(int score) //check to see if player has blackjack with initial hand
 		int dealer_swap(); //changes the dealer
 		void hit_me(); //take a card from the deck
-		void sit(); //finish turn and go to next player
+		int sit(); //finish turn and go to next player
 		~player();
 };
 
